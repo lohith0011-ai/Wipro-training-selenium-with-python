c7bb281d6d581800975c6f1f79bbe3d8da2913d8
lohith0011-ai <lohithkumarkosuru@gmail.com>
2026-02-23 18:16:23 +0530

    Delete FixtureDependency.py

--- FixtureDependency.py
+++
@@ -1,7 +0,0 @@
-# fixture dependency - the second fixture will work only when the first
-
-import pytest
-def test(fixture_b):
-    assert fixture_b == "Data from A + Data from B"
-
-#fixture_b automatically receives the value returned by fixture_a
